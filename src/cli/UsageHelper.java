87556edfdf234ee0b698d40f03f8236e7cea9cda
Oleksii <156600006+sokradd@users.noreply.github.com>
2024-10-21 10:12:49 +0300

    Delete src directory

--- src/cli/UsageHelper.java
+++
@@ -1,8 +0,0 @@
-package cli;
-
-public class UsageHelper {
-   public static void printUsage() {
-       System.out.println("itinerary usage:");
-       System.out.println("$ java Prettifier.java ./input.txt ./output.txt ./airport-lookup.csv");
-    }
-}
